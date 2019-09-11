f8e3f1dc0efe9ee1fec9b6a0e6ef37422757ad71
mingaifan <1599419227@qq.com>
2019-09-11 08:33:32 +0800

    min develop

---
+++ index.js
@@ -0,0 +1,8 @@
+// git clone +url 克隆地址到本地
+// git add file . 提交文件到暂存区
+// git commit -m 'msg' 提交文件到本地仓库
+// git push origin master 提交文件到远程仓库
+// git log 查看历史
+// git reflog 查看日志
+// git diff 比较不同
+// git reset 切换版本
