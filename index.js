18eb8096c92e52f4ddbe614e91bdb16ef6fe30c8
mingaifan <1599419227@qq.com>
2019-09-12 10:05:46 +0800

    js

--- index.js
+++ index.js
@@ -6,3 +6,9 @@
 // git reflog 查看日志
 // git diff 比较不同
 // git reset 切换版本
+
+// git branch 查看分支，创建分支
+// git checkout 切换分支
+// git checkout -b 创建并切换分支
+// git branch -d 在主分支上删除本地分支
+// git push -d origin dev 在dev分支上删除远程分支
